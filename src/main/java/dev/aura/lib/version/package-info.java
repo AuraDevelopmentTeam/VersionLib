0df7c4dc4f7cbb5f3090b37e34b056da9ff3f92a
BrainStone <yannick@tedworld.de>
2017-07-28 00:24:06 +0200

    More documentation

--- src/main/java/dev/aura/lib/version/package-info.java
+++ src/main/java/dev/aura/lib/version/package-info.java
@@ -1,5 +1,15 @@
 /**
  * This package contains all classes of the VersionLib library.<br>
- * The class you want to use is the {@link Version} class.
+ * The class you want to use is the {@link dev.aura.lib.version.Version}
+ * class.<br>
+ * <br>
+ * Example usage:
+ * 
+ * <pre>
+ * Version version1 = new Version("1.2.3");
+ * Version version2 = new Version("1.3.0");
+ * 
+ * version1.compareTo(version2); // returns a value less than 0
+ * </pre>
  */
 package dev.aura.lib.version;
